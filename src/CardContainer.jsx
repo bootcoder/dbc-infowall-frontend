03469f930a0f2f3fdc8998abaf32e3f06b8fea7e
BootCoder <bootcoder@gmail.com>
2016-11-26 11:26:38 -0800

    WIP cardcontainer

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class CardContainer extends Component {
   render () {
+    const getNames = function () {
+      return 'My Name is function'
+    }
     return (
       <div className='CardContainer'>
-        {}
+        <p>Hello From the card container</p>
+        <p>{getNames()}</p>
       </div>
     )
   }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import './stylesheets/Header.css'
+const Clock = require('react-clock')
 
 class Header extends Component {
   render () {
     return (
-      <header className='App'>
-        <p>This is the header</p>
+      <header className='Header'>
+        <div classname='header-container'>
+          <img className='brand-logo header-element float-left' src={require('./images/layout/dbc-logo-new.png')} alt='brand-logo' />
+          <div className='header-clock float'>
+            <Clock />
+          </div>
+          <img className='brand-logo header-element float-right' src={require('./images/layout/dbc-logo-new.png')} alt='brand-logo' />
+        </div>
       </header>
     )
   }
