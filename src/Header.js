6f2af9e85fe86a0dd6283e2d65dd600b2ce83771
BootCoder <bootcoder@gmail.com>
2016-11-26 13:48:19 -0800

    add react-bootstrap

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import './stylesheets/Header.css'
+import Col from 'react-bootstrap/lib/Col'
 const Clock = require('react-clock')
 
 class Header extends Component {
   render () {
     return (
-      <header className='Header'>
-        <div className='header-container'>
-          <img className='header-element header-logo' src={require('./images/layout/dbc-logo-new.png')} alt='brand-logo' />
-          <div className='header-element header-clock'>
-            <Clock />
+      <nav className='navbar navbar-inverse'>
+        <div className=''>
+          <div className='row'>
+            <Col xs={1} md={4}>
+              <img className='navbar-logo' src={require('./images/layout/dbc-logo-new.png')} alt='brand-logo' />
+            </Col>
+            <Col xs={1} md={4} className='white-text'>
+              <Clock />
+            </Col>
+            <Col xs={1} md={4} className='white-text'>
+              <Clock />
+            </Col>
           </div>
         </div>
-      </header>
+      </nav>
     )
   }
 }
